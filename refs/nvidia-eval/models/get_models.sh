c6edbfd34659de3f370e16206dbe04fa5c601938
x0rzkov <x0rzkov@protonmail.com>
2020-03-30 13:32:41 +0200

    add dockerfile, update get_models script to avoid duplicate downloads

--- refs/nvidia-eval/models/get_models.sh
+++ refs/nvidia-eval/models/get_models.sh
@@ -1,4 +1,17 @@
+#!/bin/sh
+set 
 echo "Downloading..."
-wget https://storage.googleapis.com/keymoji-data/emotions-models/ama_32k_tokenizer.model
-wget https://storage.googleapis.com/keymoji-data/emotions-models/optimized_model.pth
+
+if [ ! -f "ama_32k_tokenizer.model" ]; then
+   wget --no-check-certificate https://storage.googleapis.com/keymoji-data/emotions-models/ama_32k_tokenizer.model
+else
+  echo "already downloaded ama_32k_tokenizer.model"
+fi
+
+if [ ! -f "optimized_model.pth" ]; then
+  wget --no-check-certificate https://storage.googleapis.com/keymoji-data/emotions-models/optimized_model.pth
+else
+  echo "already downloaded optimized_model.pth"
+fi
+
 echo "Done!"
